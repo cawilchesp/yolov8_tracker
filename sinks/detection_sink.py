591e521c83e280ca99a11ce168b5d4953a2404b4
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-06-10 20:56:13 -0500

    Update detection_sink.py

--- sinks/detection_sink.py
+++ sinks/detection_sink.py
@@ -12,14 +12,12 @@ class DetectionSink:
         weights_path: str,
         image_size: int = 640,
         confidence: float = 0.5,
-        class_filter: List[int] = None,
-        nms_threshold: float = 0.7
+        class_filter: List[int] = None
     ) -> None:
         self.model = YOLO(weights_path)
         self.image_size = image_size        
         self.confidence = confidence
         self.class_filter = class_filter
-        self.nms_threshold = nms_threshold
 
     def detect(self, image: np.array) -> sv.Detections:
         results = self.model(
@@ -30,6 +28,6 @@ class DetectionSink:
             device='cuda' if torch.cuda.is_available() else 'cpu',
             verbose=False,
         )[0]
-        detections = sv.Detections.from_ultralytics(results).with_nms(threshold=self.nms_threshold)
+        detections = sv.Detections.from_ultralytics(results)
         
         return detections
