84a3dcfa60b280d96df4dcf84908a90e90b1179c
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-10-01 18:56:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,18 @@ from pathlib import Path
 ROOT = Path('D:/Data')
 SOURCE_FOLDER = ROOT
 OUTPUT_FOLDER = ROOT
-MODEL_FOLDER = ROOT / 'models' / 'yolov8'
 
 # Source parameters
-INPUT_VIDEO = 'C3_detenido.mp4'
-OUTPUT_NAME = 'C3_detenido_yolov8'
+
+INPUT_VIDEO = 'CHICO1_1_new.mp4'
+OUTPUT_NAME = 'CHICO1_1_new_yolov10'
 
 # Deep Learning model configuration
-MODEL_WEIGHTS = 'yolov8x.pt'
+MODEL_ROOT = Path('D:/Data/models')
+MODEL_FOLDER = MODEL_ROOT / 'yolo11'
+MODEL_WEIGHTS = 'yolo11m.pt'
 
 # Inference configuration
 CLASS_FILTER = [0,1,2,3,5,7]
 IMAGE_SIZE = 640
-CONFIDENCE = 0.25
+CONFIDENCE = 0.5
