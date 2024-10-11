96d1f5b82ffc0b1a11ea2539c48d5bbce49cb609
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-10-11 14:39:42 -0500

    Update zone_analysis.py

--- modules/zone_analysis.py
+++ modules/zone_analysis.py
@@ -5,6 +5,7 @@ import json
 import torch
 import numpy as np
 from typing import List
+from shapely.geometry import Point, Polygon
 
 # For debugging
 from icecream import ic
@@ -18,6 +19,7 @@ class ZoneAnalysis:
         self.json_path = json_path
         self.json_data = self.load_json(json_path)
         self.polygons = [np.array(polygon, np.int32) for polygon in self.json_data['zones']]
+
         self.calibration_zone = np.array(self.json_data['calibration']['zone'])
         self.calibration_width = self.json_data['calibration']['width']
         self.calibration_height = self.json_data['calibration']['height']
@@ -35,8 +37,59 @@ class ZoneAnalysis:
             data = json.load(file)
 
             return data
+        
+    def orientation(self, new_position: tuple, last_position: tuple) -> tuple:
+        _, cx1, cy1, _, _, _ = last_position
+        cx2, cy2 = new_position
+
+        orientation_x = cx2 - cx1
+        orientation_y = cy2 - cy1
+
+        return (orientation_x, orientation_y)
     
-    def on_zones(self, scene: np.array) -> np.array:
+    def invasion_estimation(self, center: tuple[float, float], orientations: tuple[float, float]):
+        cx, cy = center
+        orientation_x, orientation_y = orientations
+
+        inside_1 = Polygon(self.zones[0].polygon.tolist()).contains(Point(cx, cy))
+        # inside_2 = Polygon(self.zones[1].polygon.tolist()).contains(Point(cx, cy))
+
+        direction_x = 0
+        if inside_1:
+            direction_y = orientation_y * -1
+        # elif inside_2:
+        #     direction_y = orientation_y * 1
+        else:
+            direction_y = 0
+        
+        return (direction_x, direction_y)
+    
+    def annonate_alert(self, position_track: dict, scene: np.array) -> np.array:
+        for object_position in position_track.values():
+            _, (x1, y1, x2, y2), directions = object_position
+            
+            if directions[0] < 0 or directions[1] < 0:
+                cv2.rectangle(
+                    img=scene,
+                    pt1=(int(x1), int(y1)),
+                    pt2=(int(x2), int(y2)),
+                    color=(0,0,255),
+                    thickness=2,
+                )
+
+                scene = sv.draw_text(
+                    scene=scene,
+                    text="Invasion de Carril",
+                    text_anchor=sv.Point(540, 50),
+                    background_color=sv.Color.RED,
+                    text_color=sv.Color.WHITE,
+                    text_scale=2,
+                    text_thickness=2
+                )
+
+        return scene
+    
+    def annotate_zones(self, scene: np.array) -> np.array:
         mask_image = scene.copy()
         COLORS = sv.ColorPalette.from_hex(["#FFFF55", "#3C76D1", "#FFFF55", "#FF5555", "#3CB44B"])
         
